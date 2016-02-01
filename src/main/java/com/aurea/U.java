c824866765432032aa22f870e68a2d90f5a88dec
Arsen A. Gutsal <gutsal.arsen@softsky.com.ua>
2016-02-01 11:42:33 +0200

    added neo4j deps and trying to use it

--- src/main/java/com/aurea/U.java
+++ src/main/java/com/aurea/U.java
@@ -7,16 +7,27 @@ import com.scitools.understand.Database;
 import com.scitools.understand.Entity;
 
 import com.scitools.understand.UnderstandException;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 public class U {
 
     public static void main(String[] args) throws UnderstandException {
 	System.out.println(System.getenv());
 	Understand.loadNativeLibrary();
-	Database db = Understand.open("./target/testDB.udb");
+	Database db = Understand.open("./savvion.udb");
 
-	for(Entity o: db.ents("class, function")){
+	GraphDatabaseService gdb = new GraphDatabaseFactory().newEmbeddedDatabase();
+
+	try ( Transaction tx = graphDb.beginTx() ){
+		// Database operations go here
+		tx.success();
+	} catch(Exception ex){
+	    // TODO
+	}
+
+	for (Entity o : db.ents("class, function")) {
 	    System.out.println(o.name() + ":" + Arrays.toString(o.metrics()));
 	};
-    }    
+    }
 }
