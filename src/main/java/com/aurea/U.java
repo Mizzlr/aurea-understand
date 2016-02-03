59b69b4459b9a2e0ec1845b65f89253311909ce3
Jett Gamboa <jett.gamboa@aurea.com>
2016-02-03 14:38:33 +0800

    modified U sample to retrieve methods and parameters

--- src/main/java/com/aurea/U.java
+++ src/main/java/com/aurea/U.java
@@ -2,32 +2,36 @@ package com.aurea;
 
 import java.util.Arrays;
 
-import com.scitools.understand.Understand;
-import com.scitools.understand.Database;
-import com.scitools.understand.Entity;
+import com.scitools.understand.*;
 
-import com.scitools.understand.UnderstandException;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 public class U {
 
-    public static void main(String[] args) throws UnderstandException {
-	System.out.println(System.getenv());
-	Understand.loadNativeLibrary();
-	Database db = Understand.open("./savvion.udb");
+	public static void main(String[] args) throws UnderstandException {
+//	System.out.println(System.getenv());
+		Understand.loadNativeLibrary();
+		Database db = Understand.open("./target/MyUnderstandProject.udb");
 
-	GraphDatabaseService gdb = new GraphDatabaseFactory().newEmbeddedDatabase();
+//	GraphDatabaseService gdb = new GraphDatabaseFactory().newEmbeddedDatabase();
+//
+//	try ( Transaction tx = graphDb.beginTx() ){
+//		// Database operations go here
+//		tx.success();
+//	} catch(Exception ex){
+//	    // TODO
+//	}
 
-	try ( Transaction tx = graphDb.beginTx() ){
-		// Database operations go here
-		tx.success();
-	} catch(Exception ex){
-	    // TODO
+		// for (Entity o : db.ents("class, function")) {
+		for (Entity method : db.ents("Method")) {
+//	    System.out.println(o.name() + ":" + Arrays.toString(o.metrics()));
+			System.out.println(method.longname(true));
+
+			for(Reference param : method.refs("Define","", true)) {
+				System.out.println("\t" + param.ent().type() + " " + param.ent().name());
+			}
+		};
+
 	}
-
-	for (Entity o : db.ents("class, function")) {
-	    System.out.println(o.name() + ":" + Arrays.toString(o.metrics()));
-	};
-    }
 }
